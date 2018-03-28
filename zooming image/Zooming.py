5d84829bf69f6c9685352a5a340625cc2be82f22
thenelman <37222454+thenelman@users.noreply.github.com>
2018-03-28 16:22:36 +0530

    update

--- zooming image/Zooming.py
+++ zooming image/Zooming.py
@@ -7,34 +7,43 @@ Created on Tue Mar 27 20:39:45 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-def imageZoom(im,scale): # zooms the image by scale times
+class Pivot: 
+    "class to store the co ordinate of the pivot"
+    """pivot row position should lie between [row/2] and [(k*(row-1)+1) - (row/2)]"""
+    """pivot column position should lie between [column / 2] and [(k*(column-1)+1) - (column / 2)]"""
+
+    def __init__(self,rw,cl):
+        self.rw = rw #row position
+        self.cl = cl #col position
+        
+def imageZoom(im,scale,pivot): # zooms the image by scale times
     """Zooms the RGB image"""
+    
     blue_im = im[:,:,0]
     red_im = im[:,:,1]
     green_im = im[:,:,2]
     row, col, dim = im.shape
-    zoomed_blue = zoom(blue_im,scale)
-    zoomed_red = zoom(red_im,scale)
-    zoomed_green = zoom(green_im,scale)
+    zoomed_blue = zoom(blue_im,scale,pivot)
+    zoomed_red = zoom(red_im,scale,pivot)
+    zoomed_green = zoom(green_im,scale,pivot)
+    #recombine the pixels
     zoomedImage = np.array([[[zoomed_blue[i,j],zoomed_red[i,j],zoomed_green[i][j]]for j in range(col)]for i in range(row)])
     return zoomedImage
 
-def zoom(image,scale): #helper function to zoom the pixels of each color
+def zoom(image,scale,pivot): #helper function to zoom the pixels of each color
     row , column = image.shape
-    imageList = image.tolist()
+    imageList = image.tolist() # convert the numpy.ndarray to a list object
     r = rowOperation(imageList,0,scale)
     i = 0
     while(i  <  len(imageList)-1):
         rowOperation(imageList,i+1,scale)
         colOperation(imageList,i,scale)
         i = i+scale
-    midRow = ((scale*(row-1))+1)//2
-    midCol =((scale*(column-1))+1)//2
-    startRow = midRow - (row//2)
+    startRow = pivot.rw - (row//2)
     endRow = startRow + row
-    startCol = midCol - (column//2)
+    startCol = pivot.cl - (column//2)
     endCol = startCol + column
-    newImage = np.array(imageList,dtype=np.uint8)
+    newImage = np.array(imageList,dtype=np.uint8) #convert back to numpy.ndarray
     return newImage[startRow:endRow,startCol:endCol]
 
 
@@ -89,15 +98,21 @@ def main():
     ImageFilename = input("Enter image path")
     image = plt.imread(ImageFilename)
     k = int(input("Enter zooming factor")) #zooming factor
+    """pivot row position should lie between [row/2] and [(k*(row-1)+1) - (row/2)]"""
+    """pivot column position should lie between [column / 2] and [(k*(column-1)+1) - (column / 2)]"""
+    rw = int(input("Enter the pivot row position. ")) 
+    cl = int(input("Enter the pivot column position. "))
+    pivot = Pivot(rw,cl)
     
     #show original image
     plt.imshow(image)
     plt.show()
     
     #Zoom the image by k times
-    newImage = imageZoom(image,k)
+    newImage = imageZoom(image,k,pivot)
     
     #shape of the zoomed image
+    #must be of the same size of the original image
     print(newImage.shape)
     
     #show the new zoomed image
